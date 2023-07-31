cb8e21cccbd0da8cba7dc417c4169657ba2072f9
Jerry <jerry.clou@outlook.com>
2023-07-31 10:49:53 +0200

    seconde commit

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
 use App\Repository\ProduitRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +11,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PanierController extends AbstractController
 {
-    #[Route('/panier', name: 'app_panier')]
-    public function index(): Response
+    #[Route('panier', name: 'app_panier')]
+    public function index(SessionInterface $session, ProduitRepository $productRepository): Response
     {
+        $cart = $session->get('cart');
+        $total = 0;
+
+        $dataCart = [];
+        foreach ($cart as $id => $quantity) {
+            $product = $productRepository->find($id);
+            $dataCart[] = [
+                "product" => $product,
+                'quantity' => $quantity
+            ];
+            $total += $product->getPrix() * $quantity;
+        }
         return $this->render('panier/index.html.twig', [
-            'controller_name' => 'PanierController',
+            'dataCart' => $dataCart,
+            'total' => $total
         ]);
     }
 
@@ -28,26 +42,40 @@ class PanierController extends AbstractController
             $cart[$id] = 1;
         }
         $session->set('cart', $cart);
-        return $this->render('panier/index.html.twig', [
-            'cart' => $cart,
-        ]);
+        return $this->redirectToRoute("app_panier");
     }
 
-    #[Route('/read', name: 'app_read_panier')]
-    public function read(SessionInterface $session, ProduitRepository $productRepository): Response
+    #[Route('/remove/{id}', name: 'app_remove_panier')]
+    public function remove(SessionInterface $session, $id): Response
     {
         $cart = $session->get('cart');
-        $total = 0;
-        // foreach ($cart as $id => $quantity) {
-        //     $product = $productRepository->find($id);
-        //     $dataCart[] = [
-        //         "product" => $product,
-        //         'quantity' => $quantity
-        //     ];
-        //     $total += $product->getPrice() * $quantity;
-        // }
-        return $this->render('panier/index.html.twig', [
-            'cart' => $cart,
-        ]);
+        if (!empty($cart[$id])) {
+            if ($cart[$id] > 1) {
+                $cart[$id]--;
+            } else {
+                unset($cart[$id]);
+            }
+        }
+        $session->set('cart', $cart);
+        return $this->redirectToRoute("app_panier");
+    }
+
+    #[Route('/delete/{id}', name: 'app_delete_panier')]
+    public function delete(SessionInterface $session, $id): Response
+    {
+        $cart = $session->get('cart');
+        if (!empty($cart[$id])) {
+            unset($cart[$id]);
+        }
+        $session->set('cart', $cart);
+        return $this->redirectToRoute("app_panier");
+    }
+
+    #[Route('/flush', name: 'app_flush_panier')]
+    public function flush(SessionInterface $session): Response
+    {
+        $cart = $session->set('cart', []);
+        $session->set('cart', $cart);
+        return $this->redirectToRoute("app_home");
     }
 }
